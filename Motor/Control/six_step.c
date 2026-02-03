b405e469b21144358c7acf7956e68319c1e193b6
Minhwihong <34900909+Minhwihong@users.noreply.github.com>
2026-02-03 17:31:09 +0900

    홀센서 노이즈 필터링 강화2

--- Motor/Control/six_step.c
+++ Motor/Control/six_step.c
@@ -252,8 +252,18 @@ uint8_t Check_Valid_HallCode(uint8_t state){
 	static volatile uint32_t g_last_tick = 0;
 
 
+	uint32_t t = TIM7->CNT;
+    uint32_t dt = t - g_last_tick;
+
+    if (dt < 5) { // 10us 이내 재발생은 노이즈로 간주 (초기값 예시)
+        return 0;
+    }
+
+    g_last_tick = t;
+
+
 	/* ****************************************************************
-	1-bit만 바뀌었는지(그레이 코드) 체크: prev^now의 비트 수가 1인지
+	1-bit만 바뀌었는지(그레이 코드) 체크: prev XOR now의 비트 수가 1인지
 	정상적인 6스텝 BLDC 홀 전이에서는 “한 번에 오직 1비트만 바뀐다”
 	따라서 이전 상태와 현재 상태의 XOR 결과는 반드시 다음과 같다.
 	001 (1비트 변화)
@@ -272,15 +282,8 @@ uint8_t Check_Valid_HallCode(uint8_t state){
     }
 
 
-	uint32_t t = TIM7->CNT;
-    uint32_t dt = t - g_last_tick;
 
-    if (dt < 5) { // 10us 이내 재발생은 노이즈로 간주 (초기값 예시)
-        return 0;
-    }
-
-    g_last_tick = t;
-
+	g_hall_prev = state;
 
 	return 1;
 }
